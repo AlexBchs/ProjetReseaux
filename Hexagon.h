0f291c60c37c5a1dd3745ebe3670911ee2b3ef16
Lukas <areolame@gmail.com>
2022-01-15 18:35:32 +0100

    Change Pi by Pi from Math.h

--- Hexagon.h
+++ Hexagon.h
@@ -2,6 +2,7 @@
 #define HEXAGON_H
 
 #include "point.h"
+#include "math.h"
 #include <vector>
 
 
@@ -30,7 +31,7 @@ class Hexagon
 
     private :
         std::vector<Point> _listPoint;
-        static constexpr double const& PI = 3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211706798214808651328230664709384460955058223172535940812848111745028410270193852110555964462294895493038196;
+        static constexpr double const& PI = M_PI;
         double _radius;
         std::vector<double> _color;
         double _transparance;
