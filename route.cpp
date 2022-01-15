92e119b922f912342213b2608997336c78be154e
Lukas <areolame@gmail.com>
2022-01-15 19:28:11 +0100

    Some Clarification on routes

--- route.cpp
+++ route.cpp
@@ -8,7 +8,6 @@ using std::endl;
 
 Route::Route(int id, Node* node1, Node* node2) : _id{ id }, _node1 { node1 }, _node2{ node2 }
 {
-    //Add the current route to the nodes
     _node1->getConnectedRoutes().push_back(this);
     _node2->getConnectedRoutes().push_back(this);
 }
@@ -36,16 +35,34 @@ Node* Route::getNode2()
 
 int Route::toX(Node* startingNode) const
 {
-    if(startingNode == _node1) return _node2->getX() - _node1->getX();
-    else if(startingNode == _node2) return _node1->getX() - _node2->getX();
-    else return 0;
+    if(startingNode == _node1)
+    {
+        return _node2->getX() - _node1->getX();
+    }
+    else if(startingNode == _node2)
+    {
+        return _node1->getX() - _node2->getX();
+    }
+    else
+    {
+        return 0;
+    }
 }
 
 int Route::toY(Node* startingNode) const
 {
-    if(startingNode == _node1) return _node2->getY() - _node1->getY();
-    else if(startingNode == _node2) return _node1->getY() - _node2->getY();
-    else return 0;
+    if(startingNode == _node1)
+    {
+        return _node2->getY() - _node1->getY();
+    }
+    else if(startingNode == _node2)
+    {
+        return _node1->getY() - _node2->getY();
+    }
+    else
+    {
+        return 0;
+    }
 }
 
 double Route::nodesDistance() const
